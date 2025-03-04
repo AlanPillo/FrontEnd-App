7f768ca1bd636cf0796186cc166751ee1133023a
Semental <112137783+AlanPillo@users.noreply.github.com>
2025-03-04 06:22:20 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // URL del backend
+  baseURL: 'https://sistemadentistcitas.onrender.com', // URL del backend
   headers: {
     'Content-Type': 'application/json'
   }
